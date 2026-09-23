305b236e573ce91d9e5145adbb30e5e250890163
Reez <reezpatel@gmail.com>
2026-09-23 23:13:21 +0530

    flake v0.0.10

--- flake.nix
+++ flake.nix
@@ -86,7 +86,7 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        version = "0.0.9";
+        version = "0.0.10";
         serverSrc = pkgs.fetchurl {
           url = "https://github.com/reezpatel/twodb/releases/download/v${version}/twodb-server-v${version}.tar.gz";
           sha256 = "0p06ip4ajhfxxf9ym29zbjwswsn5d8z47vlknycdca1axqa82bv0";
