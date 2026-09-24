e178a1bb7e5f171fc08977d35d18931417625a23
Reez <reezpatel@gmail.com>
2026-09-24 08:55:38 +0530

    flake: v0.0.13 server hash

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@
         version = "0.0.13";
         serverSrc = pkgs.fetchurl {
           url = "https://github.com/reezpatel/twodb/releases/download/v${version}/twodb-server-v${version}.tar.gz";
-          sha256 = "1p51v5s845j738v4r96aff788shmdvpz397h9pwp03ilwrp4bhm5";
+          sha256 = "0swr8lkr9d5y5x0nwxcwp0zg2f088swjfvlz2ypvbqipdbmsrp6n";
         };
         nodeAsset =
           {
